4c5ead4fbc502267d35c9699464272c868f32804
Ehretz Geskin <sg3584@alum.barnard.edu>
2024-08-06 14:59:21 -0400

    Completing Exercise

--- serial.py
+++ serial.py
@@ -19,4 +19,21 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start):
+        "Create serial number."
+        self.start = start
+        self.first = start
 
+    def __repr__(self):
+        return f"<SerialGenerator start={self.start} next={self.start + 1}>"
+
+    def generate(self):
+        "Generate serial number (from start)."
+        num = self.start
+        self.start += 1
+        return num
+    
+    def reset(self):
+        "Resets serial number to starting number."
+        self.start = self.first
+    
--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,46 @@
 """Word Finder: finds random words from a dictionary."""
 
+import random
 
 class WordFinder:
-    ...
+    """Finds words from a given txt document."""
+
+    def __init__(self, path):
+        "Creates a WordFinder object with the given path to a txt document."
+        self.path = path
+        self.words = self.make_list()
+        self.num_words()
+
+    def make_list(self):
+        "Makes an attribute list of the words found in the txt document." 
+        words = []
+        file = open(self.path, 'r')
+        for line in file:
+            line = line.strip()
+            words.append(line)
+        file.close()
+        return words
+    
+    def num_words(self):
+        "Returns the number of words found in the given txt document."
+        num_words = len(self.words)
+        print(f"{num_words} words read")
+
+    def random(self):
+        "Returns a random word from the list of words created from the txt document."
+        return random.choice(self.words)
+
+class SpecialWordFinder(WordFinder):
+    """Finds words from a given txt document that has comments and spaces"""
+    def __init__(self, path):
+        super().__init__(path)
+    
+    def make_list(self):
+        words = []
+        file = open(self.path, 'r')
+        for line in file:
+            line = line.strip()
+            if line and line[0].isalpha():
+                words.append(line)
+        file.close()
+        return words
